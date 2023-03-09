52d191b8e2751da3fd1906c7969baba22fb31370
Vivekflutter <er.vivekpatil@gmail.com>
2023-03-09 16:50:10 +0530

    Update2

--- FormApp/urls.py
+++ FormApp/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from FormApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name="Home"),
     path('EventForm', views.createForm, name="Form")
 ]
